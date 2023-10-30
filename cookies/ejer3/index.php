d13b4cb7f054a56b1bb39ef8da1064983f93056e
Anartz <anartz.pagaldai@ikasle.egibide.org>
2023-10-30 10:58:42 +0100

    ejer4 cookie

--- cookies/ejer3/index.php
+++ cookies/ejer3/index.php
@@ -1,3 +1,8 @@
 <?php
+$idioma = isset($_COOKIE["idioma"])? $_COOKIE["idioma"] : "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $idioma = $_POST["idioma"];
+    setcookie("idioma", $_POST["idioma"], time() + 60);
+}
 
 require "index.view.php";
--- cookies/ejer3/index.view.php
+++ cookies/ejer3/index.view.php
@@ -6,8 +6,13 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>idioma: <?= $idioma ?></h3>
     <form action="" method="post">
-        
+        <select name="idioma">
+            <option value="euskera">euskera</option>
+            <option value="castallano">castallano</option>
+        </select>    
+        <input type="submit">
     </form>
 </body>
 </html>
